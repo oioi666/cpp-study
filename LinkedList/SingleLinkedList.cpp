1afe4df8968d40efe06d1ff76e914f7f65000468
oioi <1735304182@qq.com>
2026-06-09 19:05:02 +0800

    该单链表可以实现初始化，用前插法插入数据和遍历整个链表的功能

---
+++ LinkedList/SingleLinkedList.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#define OK 1
+using namespace std;
+
+typedef int Status;
+typedef struct LNode{
+    int data;
+    struct LNode *next;
+}LNode,*LinkList;
+
+Status InitList(LinkList &L){
+    L=new LNode;
+    L->next=NULL;
+    return OK;
+}
+//前插法创建链表
+void CreateList(LinkList &L,int n){
+    for(int i=0;i<n;i++){
+        LNode *p= new LNode;//如果只是定义为一个指针，则表示他可以指向此类结构体，但他现在没有指向任何节点
+        cin>>p->data;
+        p->next=L->next;
+        L->next=p;
+    }
+}
+//输出链表里面的数据
+void Traverse(LinkList L){
+    LNode *p;
+    p=L->next;
+    while(p!=NULL){ //注意指针p的判断条件
+    cout<<p->data<<" ";
+    p=p->next;
+    }
+}
+
+int main(){
+    LinkList l;
+    if(InitList(l)) cout<<"初始化正常";
+    int n;
+    cin>>n;
+    CreateList(l,n);
+    Traverse(l);
+}
