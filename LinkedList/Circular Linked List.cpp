11dde0f1215ad568f2a190478a548b13ec15ac16
oioi <1735304182@qq.com>
2026-06-14 21:46:02 +0800

    新增了遍历循环链表的函数（将头节点的数据设为NULL）

--- LinkedList/Circular Linked List.cpp
+++ LinkedList/Circular Linked List.cpp
@@ -32,7 +32,7 @@ void CreateList(LinkList &L,int m){
 void TraverseList(LinkList L){
     LNode *p;
     p=L->next;
-    while(p){
+    while(p->data){
         cout<<p->data<<" ";
         p=p->next;
     }
@@ -51,6 +51,7 @@ void CircularList(LinkList &L){
 int main(){
     LinkList l;
     InitList(l);
+    l->data=NULL;
     int n;
     cin>>n;
     CreateList(l,n);
